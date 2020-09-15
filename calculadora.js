7446ea103ccb52c6b0e7729fefed0200e8d0d579
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2020-09-15 13:38:03 -0300

    Adicionado a versao 1.0 do projeto calculadora

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+function calcular (tipo, valor) {
+    if (tipo === 'acao') {
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor; 
+        }
+        if (valor === 'c') {
+            document.getElementById('resultado').value = '';
+        }
+    }
+     if (valor === '='){
+       var campo = eval(document.getElementById('resultado').value);
+       document.getElementById('resultado').value = campo;
+       console.log(campo); 
+    }
+    else if(tipo === 'valor'){
+        document.getElementById('resultado').value += valor;
+    };
+  
+}
